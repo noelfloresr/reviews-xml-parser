5fd8e295cd05e1e9899e6b724d2a97260c43041c
Noel Flores <noelfloresr@gmail.com>
2024-11-01 14:53:48 -0400

    improve project

--- transformXML.js
+++ transformXML.js
@@ -1,38 +1,72 @@
 const fs = require("fs");
 const { parseStringPromise, Builder } = require("xml2js");
+const products = require("./products.json");
 
+let reviewId = 1;
+
+function getFirstSentence(paragraph) {
+  const match = paragraph.match(/(.*?[\.\?\!])\s/);
+  return match ? match[1] : paragraph; // If no match, return full paragraph as it's only one sentence.
+}
+
+function getProductUrl(product_id) {
+  const product = products.find((p) => p.product_id === product_id);
+  return product ? product.confirmed_url : null;
+}
+
+const getProductSku = (product_id) => {
+  const product = products.find((p) => p.product_id === product_id);
+  return product ? product.sku : null;
+};
 async function transformXML(inputFile, outputFile) {
   try {
-    // Leer y parsear el archivo XML
     const data = fs.readFileSync(inputFile, "utf8");
     const parsedData = await parseStringPromise(data, { explicitArray: false });
 
-    // Extraer y transformar los datos según el nuevo formato
-    const reviews = parsedData.reviews.review.map((review) => ({
-      reviewer: {
-        name: review["g:reviewer_name"],
-        reviewer_id: review["g:reviewer_id"],
-      },
-      review_timestamp: new Date(review["g:review_timestamp"]).toISOString(),
-      content: review["g:review_content"],
-      review_url: {
-        $: { type: "singleton" }, // Definir "type" como atributo
-        _: review["g:product_url"], // Definir el contenido de la etiqueta
-      },
-      ratings: {
-        overall: {
-          $: { min: "1", max: "5" }, // Definir "min" y "max" como atributos
-          _: review["g:review_rating"], // Definir el valor de la etiqueta
-        },
-      },
-      products: {
-        product: {
-          product_url: review["g:product_url"],
-        },
-      },
-    }));
+    // Build the new reviews
 
-    // Construir la nueva estructura XML
+    const reviews = parsedData.reviews.review.map((review) => {
+      isValidProductUrl = getProductUrl(review["g:product_id"]);
+
+      if (isValidProductUrl) {
+        return {
+          review_id: reviewId++,
+          reviewer: {
+            name: review["g:reviewer_name"],
+            reviewer_id: review["g:reviewer_id"],
+          },
+          review_timestamp: new Date(review["g:review_timestamp"]).toISOString(),
+          title: getFirstSentence(review["g:review_content"]),
+          content: review["g:review_content"],
+          review_url: {
+            $: { type: "singleton" }, // Definir "type" como atributo
+            _: review["g:product_url"], // Definir el contenido de la etiqueta
+          },
+          ratings: {
+            overall: {
+              $: { min: "1", max: "5" }, // Definir "min" y "max" como atributos
+              _: review["g:review_rating"], // Definir el valor de la etiqueta
+            },
+          },
+          products: {
+            product: {
+              product_ids: {
+                skus: {
+                  sku: getProductSku(review["g:product_id"]),
+                },
+              },
+              product_url: getProductUrl(review["g:product_id"]),
+            },
+          },
+        };
+      }
+
+      return null;
+    });
+
+    const finalReviews = reviews.filter((review) => review !== null);
+
+    // Build the new XML output
     const output = {
       feed: {
         $: {
@@ -42,15 +76,15 @@ async function transformXML(inputFile, outputFile) {
             "http://www.google.com/shopping/reviews/schema/product/2.3/product_reviews.xsd",
         },
         version: "2.3",
-        aggregator: {
-          name: "Sample Reviews Aggregator (if applicable)",
-        },
+        // aggregator: {
+        //   name: "Sample Reviews Aggregator (if applicable)",
+        // },
         publisher: {
           name: "Kozie Clothes",
           favicon: "https://www.kozieclothes.com/favicon.ico",
         },
         reviews: {
-          review: reviews,
+          review: finalReviews,
         },
       },
     };
